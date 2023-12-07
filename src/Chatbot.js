30c9d8acf61ef52df4ba931e0a11733b8dd0db78
KyrieRui <kyrie19960324@gmail.com>
2023-12-07 13:12:25 +1300

    new version AI model 'llama2 7b chat' replace the GPT API

--- src/Chatbot.js
+++ src/Chatbot.js
@@ -6,63 +6,59 @@ const Chatbot = () => {
   const [input, setInput] = useState("");
   const [messages, setMessages] = useState([]);
 
-  const chatWithGPT3 = async (userInput) => {
-    const apiEndpoint =
-      "https://api.openai.com/v1/engines/davinci-codex/completions";
-    const headers = {
-      "Content-Type": "application/json",
-      // Authorization: `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`
-      Authorization: `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`,
-    };
-
-    const data = {
-      prompt: userInput,
-      max_tokens: 150,
-    };
+  const sendMessage = async (prompt) => {
     try {
-      const response = await axios.post(apiEndpoint, data, { headers });
-      return response.data.choices[0].text.trim();
+      const response = await axios.post('http://localhost:8000/complete', {
+        prompt,
+      }, {
+        headers: {
+          'Content-Type': 'application/json',
+          'Access-Control-Allow-Origin': '*'
+        }
+      });
+      const botReply = response?.data?.data?.choices?.[0]?.text;
+      if (botReply) {
+        setMessages((prevMessages) => [...prevMessages, { text: botReply, isBot: true }]);
+      }
     } catch (error) {
-      console.error("Error communicating with the API:", error.message);
-      return "";
+      console.error('Error:', error);
     }
   };
+
   const handleSubmit = async (e) => {
     e.preventDefault();
     if (!input.trim()) return;
     const userMessage = { text: input, user: true };
     setMessages((prevMessages) => [...prevMessages, userMessage]);
-    const aiMessage = { text: "...", user: false };
-    setMessages((prevMessages) => [...prevMessages, aiMessage]);
-    const response = await chatWithGPT3(input);
-    const newAiMessage = { text: response, user: false };
-    setMessages((prevMessages) => [...prevMessages.slice(0, -1), newAiMessage]);
+    sendMessage(input);
     setInput("");
   };
+
   return (
-    <div className="chatbot-container">
-      <div className="chatbot-messages">
-        {messages.map((message, index) => (
-          <div
-            key={index}
-            className={`message ${
-              message.user ? "user-message" : "ai-message"
-            }`}
-          >
-            {message.text}
-          </div>
-        ))}
+      <div className="chatbot-container">
+        <div className="chatbot-messages">
+          {messages.map((message, index) => (
+              <div
+                  key={index}
+                  className={`message ${
+                      message.user ? "user-message" : "ai-message"
+                  }`}
+              >
+                {message.text}
+              </div>
+          ))}
+        </div>
+        <form className="chatbot-input-form" onSubmit={handleSubmit}>
+          <input
+              type="text"
+              value={input}
+              onChange={(e) => setInput(e.target.value)}
+              placeholder="Type your message..."
+          />
+          <button type="submit">Send</button>
+        </form>
       </div>
-      <form className="chatbot-input-form" onSubmit={handleSubmit}>
-        <input
-          type="text"
-          value={input}
-          onChange={(e) => setInput(e.target.value)}
-          placeholder="Type your message..."
-        />
-        <button type="submit">Send</button>
-      </form>
-    </div>
   );
 };
+
 export default Chatbot;
